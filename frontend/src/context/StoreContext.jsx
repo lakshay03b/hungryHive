eaa147bfe969fb6cc97234198a6a5684ccf13ce7
Lakshay Bansal <100891199+lakshay03b@users.noreply.github.com>
2024-07-04 15:41:52 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url="http://localhost:4000"
+    const url="https://hungryhive-backend-4224.onrender.com"
     const [token,setToken] = useState("");
     const [food_list,setFoodList] = useState([])
 
